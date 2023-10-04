2e622c59597f09bb69ae41bcdbbe22c868ce8e9d
kakasy <drum2to@yandex.ru>
2023-10-04 15:48:01 +0300

    refactor: обновить CatTest

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -14,19 +14,10 @@ import java.util.List;
 public class CatTest {
 
     @Mock
-    private Cat cat;
+    private Feline feline;
 
     @Test
     public void getCatSoundTest() {
-        Mockito.when(cat.getSound()).thenReturn("Мяу");
-        String actual = cat.getSound();
-        String expected = "Мяу";
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void catSoundTest() {
-        Feline feline = new Feline();
         Cat cat = new Cat(feline);
         String actual = cat.getSound();
         String expected = "Мяу";
@@ -34,25 +25,17 @@ public class CatTest {
     }
 
     @Test
-    public void catFoodTest() {
-        try {
-            Mockito.when(cat.getFood()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
-            List<String> actual = cat.getFood();
-            List<String> expected = List.of("Животные", "Птицы", "Рыба");
-            Assert.assertEquals(expected, actual);
-        } catch (Exception e) {
-            Assert.fail("Исключение не выбрасывается при правильной работе: " + e.getMessage());
-        }
+    public void catFoodTest() throws Exception {
+            Cat cat = new Cat(feline);
+            cat.getFood();
+            Mockito.verify(feline, Mockito.times(1)).eatMeat();
     }
 
     @Test
-    public void getFoodTest() {
-        try {
-            Feline feline = new Feline();
+    public void getFoodTest() throws Exception {
             Cat cat = new Cat(feline);
-            System.out.println(cat.getFood());
-        } catch (Exception e) {
-            Assert.fail("Исключение не выбрасывается при правильной работе: " + e.getMessage());
-        }
+            List<String> expected = List.of("Животные", "Птицы", "Рыба");
+            Mockito.when(feline.eatMeat()).thenReturn(expected);
+            Assert.assertEquals(expected, cat.getFood());
     }
 }
